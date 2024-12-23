49d70864d380c456d8377241930415bfc113ff60
Aziza_Teacher <athanovaaziy@gmail.com>
2024-12-23 10:26:57 +0300

    author profiles

---
+++ src/pages/components/PriviewButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PriviewButton() {
+  return (
+    <button className='preview-button'>
+       PREVIEW
+    </button>
+  )
+}
+
+export default PriviewButton
---
+++ src/pages/components/Rating.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { IoStarSharp } from "react-icons/io5";
+
+function Rating() {
+  return (
+    <>
+      <IoStarSharp />
+      <IoStarSharp />
+      <IoStarSharp />
+      <IoStarSharp />
+      <IoStarSharp />
+    </>
+  );
+}
+
+export default Rating;
